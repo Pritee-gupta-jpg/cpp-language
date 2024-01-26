a62f65e5a82134ffe5411ea4cb98e9ebff2aa3e0
Pritee Gupta <136907754+Pritee-gupta-jpg@users.noreply.github.com>
2024-01-27 03:41:35 +0530

    Create macroSwap.cpp

---
+++ macroSwap.cpp
@@ -0,0 +1,11 @@
+
+# include<bits/stdc++.h>
+using namespace std;
+# define swapN(x,y) int temp=x; x=y; y=temp;
+int main(){
+    int a=10,b=20;
+    cout<<"value of a="<<a<<"and b="<<b<<"before swapping"<<endl;
+    swapN(a,b);
+     cout<<"value of a="<<a<<"and b="<<b<<"after swapping"<<endl;
+    
+}
