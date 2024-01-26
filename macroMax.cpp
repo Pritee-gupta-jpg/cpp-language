b49c60182736f0e760ac82910f7e52da18fe6007
Pritee Gupta <136907754+Pritee-gupta-jpg@users.noreply.github.com>
2024-01-27 03:33:52 +0530

    Create macroMax.cpp

---
+++ macroMax.cpp
@@ -0,0 +1,8 @@
+# include<bits/stdc++.h>
+using namespace std;
+# definme maxi(x,y) ((x)>(y)?(x):(y))
+int main(){
+int a=5,b=7;
+  cout<<"maximum of 5 and 7 using macro is :"<<maxi(a,b);
+  return 0;
+}
